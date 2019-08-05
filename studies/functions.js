eb027fff5d41b9165971f6a64319f3330f2e5fe2
Ubuntu <ubuntu@ip-172-31-87-127.ec2.internal>
2019-08-05 14:33:02 +0000

    studies-updates

--- studies/functions.js
+++ studies/functions.js
@@ -1,1 +1,30 @@
 //1. The two phases to using functions: First we must ___? Next we can execute (or two other words for executing a function?) a function by?
+//FUNCTION DECLERATION
+/*function NAME(Paramaters){
+*    function body
+*   return statement
+}*/
+//Function Call/Innovation/Execution
+//Name(ARGUMENTS)
+
+//2.What’s the difference between a function’s parameters and arguments PASSED to a function?
+//The parameters are like variables. Arguments are what actually gets passed throught the function
+//3. What’s the syntax for a NAMED function?
+/*function Name(Parameters){
+ *   function body
+ *  return statement    
+}
+*/
+//4. How do we assign a function to a variable?
+/*var add = function(a,b){
+ *function body
+ *return statement
+ *}
+*/
+
+//5. Functions can OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specify inputs, and how do we specify outputs?
+/*inputs are parameters and outputs are arguments(what gets changed)*/
+//6. Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
+//7. Closures: Functions form closures around the data they house. If an object returned from the Function and is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these closures! 
+
+
